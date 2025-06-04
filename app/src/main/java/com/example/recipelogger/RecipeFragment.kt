eef5def48904dd021ee74b316b9cbfb7f87cd5d0
berkeboran <sberkeborans@gmail.com>
2025-06-04 22:49:21 +0300

    add gallery image picker with runtime permission support for Android 13+ and legacy versions

--- app/src/main/java/com/example/recipelogger/RecipeFragment.kt
+++ app/src/main/java/com/example/recipelogger/RecipeFragment.kt
@@ -1,22 +1,40 @@
 package com.example.recipelogger
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.recipelogger.databinding.FragmentRecipeBinding
+import com.google.android.material.snackbar.Snackbar
 
 class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private var selectedImage: Uri? = null
+    private var selectedBitmap: Bitmap? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+        registerLauncher()
     }
 
     override fun onCreateView(
@@ -34,8 +52,78 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.imageView.setOnClickListener {  }
+        binding.imageView.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            requireActivity(),
+                            Manifest.permission.READ_MEDIA_IMAGES
+                        )
+                    ) {
+                        Snackbar.make(
+                            view,
+                            "Galireye ulaşıp görsel seçmemiz lazım!",
+                            Snackbar.LENGTH_INDEFINITE
+                        ).setAction(
+                            "İzin ver", View.OnClickListener {
+                                permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+
+                            }
+
+                        ).show()
+
+                    } else {
+                        permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                    }
+                } else {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+
+                }
+
+            } else {
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            requireActivity(),
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                    ) {
+                        Snackbar.make(
+                            view,
+                            "Galireye ulaşıp görsel seçmemiz lazım!",
+                            Snackbar.LENGTH_INDEFINITE
+                        ).setAction(
+                            "İzin ver", View.OnClickListener {
+                                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                            }
+                        ).show()
+
+                    } else {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }
+                } else {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+
+                }
+            }
+        }
         binding.saveButton.setOnClickListener {  }
+
+
+
+
         binding.deleteButton.setOnClickListener {  }
         arguments?.let {
             val information = RecipeFragmentArgs.fromBundle(it).information
@@ -49,4 +137,47 @@ class RecipeFragment : Fragment() {
 
         }
     }
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        selectedImage = intentFromResult.data
+                        try {
+                            if (Build.VERSION.SDK_INT > 28) {
+                                val source = ImageDecoder.createSource(
+                                    requireActivity().contentResolver,
+                                    selectedImage!!
+                                )
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            } else {
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver,
+                                    selectedImage
+                                )
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+                        } catch (e: Exception) {
+                            println(e.localizedMessage)
+                        }
+
+
+                    }
+                }
+            }
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    Toast.makeText(requireContext(), "İzin verilmedi!", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+
 }
